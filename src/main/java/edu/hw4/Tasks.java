e8c6ad2bdb4e3e250ca2a36d6a53b92c60f82eff
Big Shlepa <Andrsan2004@yandex.ru>
2023-11-11 23:04:33 +0300

    hw4

---
+++ src/main/java/edu/hw4/Tasks.java
@@ -0,0 +1,141 @@
+package edu.hw4;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+import java.util.Set;
+import java.util.stream.Collectors;
+import edu.hw4.ValidationError;
+
+public class Tasks {
+    //1
+    public List<Animal> sortByHeight(List<Animal> animals){
+        animals = animals.stream().sorted(Comparator.comparing(Animal::height)).toList();
+        return animals;
+    }
+    //2
+    public List<Animal> LightToHeavyWeight(List<Animal> animals, Integer k){
+        animals = animals.stream().sorted(Comparator.comparing(Animal::weight).reversed()).limit(k).toList();
+        return animals;
+    }
+
+    //3
+    public Map<Animal.Type, Integer> countTypes(List<Animal> animals) {
+        Map<Animal.Type, Integer> types = new HashMap<>();
+        animals.stream().forEach(animal -> {
+            if (types.containsKey(animal.type())) {
+                types.put(animal.type(), types.get(animal.type()) + 1);
+            } else {
+                types.put(animal.type(), 1);
+            }
+        });
+        return types;
+    }
+    //4
+    public Animal LongestName(List<Animal> animals){
+        animals = animals.stream().sorted((Animal n1, Animal n2) -> Integer.compare(n2.name().length(), n1.name().length())).limit(1).toList();
+        return animals.get(0);
+    }
+    //5
+    public Animal.Sex maleOrFemale(List<Animal> animals) {
+        Map<Animal.Sex, Integer> sexesCount = new HashMap<>();
+        sexesCount.put(Animal.Sex.M, 0);
+        sexesCount.put(Animal.Sex.F, 0);
+        animals.stream().forEach(animal ->
+            sexesCount.put(animal.sex(), sexesCount.get(animal.sex()) + 1)
+        );
+        return sexesCount.get(Animal.Sex.M) >= sexesCount.get(Animal.Sex.F) ? Animal.Sex.M : Animal.Sex.F;
+    }
+    //6
+    public Map<Animal.Type, Integer> heaviestAnimal(List<Animal> animals){
+        return animals.stream().collect(Collectors.toMap(Animal::type, Animal::weight, Integer::max));
+    }
+    //7
+    public Animal oldestK(List<Animal> animals, Integer k) {
+        return animals.stream().sorted(Comparator.comparing(Animal::age).reversed()).toList().get(k - 1);
+    }
+    //8
+    public Optional<Animal> haviestAnimalBelowK(List<Animal> animals, Integer k){
+        return animals.stream().filter(animal -> animal.height() < k).max(Comparator.comparing(Animal::weight));
+    }
+    //9
+    public Integer countPaws(List<Animal> animals){
+        return animals.stream().mapToInt(Animal::paws).sum();
+    }
+    //10
+    public List<Animal> AnimalsWithAgeNotEqualsPaws(List<Animal> animals){
+        return animals.stream().filter((animal) -> animal.age() != animal.paws()).toList();
+    }
+    //11
+    public List<Animal> canBite(List<Animal> animals){
+        return animals.stream().filter((animal -> animal.height() >100 && animal.bites())).toList();
+    }
+    //12
+    public Integer weightMoreThanHeight(List<Animal> animals) {
+        return animals.stream().filter(animal -> animal.weight() > animal.height()).toList().size();
+    }
+    //13
+    public List<Animal> nameWithMoreTwoWords(List<Animal> animals) {
+        return animals.stream().filter(animal -> animal.name().split(" ").length > 2).toList();
+    }
+    //14
+    public Boolean dogHigherThanK(List<Animal> animals, Integer k) {
+        return !(animals.stream().filter(animal -> animal.type().equals(Animal.Type.DOG) && animal.height() > k)
+            .toList().isEmpty());
+    }
+    //15
+    public Integer WeightFromKtoIlong(List<Animal> animals, Integer k, Integer i){
+        return animals.stream().filter((animal) -> ((animal.age() >= k) && (animal.age() <= i))).mapToInt(Animal::weight).sum();
+    }
+    //16
+    public List<Animal> SomeSorts(List<Animal> animals){
+        return animals.stream().sorted(Comparator.comparing(Animal::type).thenComparing(Animal::sex).thenComparing(Animal::name)).toList();
+    }
+    //17
+    public boolean isItTrueThatSpidersBiteMoreThanDogs(List<Animal> animals){
+        return animals.stream().mapToInt((animal) -> switch (animal.type()) {
+            case DOG -> (animal.bites()) ? 1 : 0;
+            case SPIDER -> (animal.bites()) ? -1 : 0;
+            default -> 0;
+        }).sum() < 0;
+    }
+    //18
+    public static Animal findHeaviestFishInLists(List<Animal>... animals) {
+        if (animals.length < 2) {
+            throw new IllegalArgumentException("There should be at least 2 lists");
+        }
+        List<Animal> animalList = Arrays.stream(animals).flatMap(List::stream).toList();
+        if (animalList.isEmpty() || animalList.contains(null)) {
+            return null;
+        }
+        return animalList.stream()
+            .filter((animal) -> animal.type() == Animal.Type.FISH)
+            .max(Comparator.comparingInt(Animal::weight))
+            .get();
+    }
+    //19
+    public static Map<String, Set<ValidationError>> animalsWithErrors(List<Animal> animalList) {
+        if ((animalList == null) || (animalList.isEmpty()) || (animalList.contains(null))) {
+            return Map.of();
+        }
+        return animalList.stream()
+            .collect(Collectors.toMap(Animal::name, ValidationError::validateAnimal))
+            .entrySet().stream()
+            .filter((entry) -> !entry.getValue().isEmpty())
+            .collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue));
+    }
+    //20
+
+    public static Map<String, String> animalsWithErrorsString(List<Animal> animalList) {
+        if ((animalList == null) || (animalList.isEmpty()) || (animalList.contains(null))) {
+            return Map.of();
+        }
+        return animalList.stream()
+            .collect(Collectors.toMap(Animal::name, ValidationError::validateAnimalString))
+            .entrySet().stream()
+            .filter((entry) -> !entry.getValue().isEmpty())
+            .collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue));
+    }
+}
---
+++ src/test/java/edu/hw4/TasksTests.java
@@ -0,0 +1,221 @@
+package edu.hw4;
+
+import org.junit.jupiter.api.Test;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+import static edu.hw4.Animal.Sex.F;
+import static edu.hw4.Animal.Sex.M;
+import static edu.hw4.Animal.Type.DOG;
+import static edu.hw4.Animal.Type.FISH;
+import static edu.hw4.Animal.Type.SPIDER;
+import static edu.hw4.Tasks.animalsWithErrors;
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class TasksTests {
+    Tasks tasks = new Tasks();
+    List<Animal> animals = createAnimals();
+
+    List<Animal> createAnimals() {
+        List<Animal> animals = new ArrayList<>();
+        animals.add(new Animal("Vasyaa", DOG, M, 10, 32, 364, true));
+        animals.add(new Animal("Petya", FISH, F, 32, 53, 243, false));
+        animals.add(new Animal("Jora", Animal.Type.BIRD, M, 53, 352, 53, false));
+        animals.add(new Animal("Liza", Animal.Type.CAT, F, 30, 90, 432, true));
+        return animals;
+    }
+
+    //task1
+    @Test
+    void sortedByHeightTest() {
+
+        List<Animal> sortedAnimals = new ArrayList<>();
+
+        sortedAnimals.add(new Animal("Vasyaa", DOG, M, 10, 32, 364, true));
+        sortedAnimals.add(new Animal("Petya", FISH, F, 32, 53, 243, false));
+        sortedAnimals.add(new Animal("Liza", Animal.Type.CAT, F, 30, 90, 432, true));
+        sortedAnimals.add(new Animal("Jora", Animal.Type.BIRD, M, 53, 352, 53, false));
+
+        assertThat(tasks.sortByHeight(animals)).isEqualTo(sortedAnimals);
+    }
+
+    //task2
+    @Test
+    void reverseSortByWeightAndChooseKTest() {
+        List<Animal> expectedList = new ArrayList<>();
+        expectedList.add(new Animal("Liza", Animal.Type.CAT, F, 30, 90, 432, true));
+        expectedList.add(new Animal("Vasyaa", DOG, M, 10, 32, 364, true));
+
+        assertThat(tasks.LightToHeavyWeight(animals, 2)).isEqualTo(expectedList);
+    }
+
+    //task3
+    @Test
+    void typesCountTest() {
+        Map<Animal.Type, Integer> expectedMap = new HashMap<>();
+        expectedMap.put(DOG, 1);
+        expectedMap.put(Animal.Type.CAT, 1);
+        expectedMap.put(Animal.Type.BIRD, 1);
+        expectedMap.put(FISH, 1);
+        assertThat(tasks.countTypes(animals)).isEqualTo(expectedMap);
+    }
+
+    //task4
+    @Test
+    void longestNameTest() {
+        Animal expected = new Animal("Vasyaa", DOG, M, 10, 32, 364, true);
+        assertThat(tasks.LongestName(animals)).isEqualTo(expected);
+    }
+
+    //task5
+    @Test
+    void maleOrFemaleTest() {
+        animals.add(new Animal("wef", DOG, M, 10, 20, 30, true));
+        assertThat(tasks.maleOrFemale(animals)).isEqualTo(M);
+    }
+
+    //task6
+    @Test
+    void heaviestAnimalOfTypes() {
+        animals.add(new Animal("wef", DOG, M, 10, 20, 1000, true));
+        animals.add(new Animal("wef", Animal.Type.BIRD, M, 10, 20, 1000, true));
+        Map<Animal.Type, Animal> expectedMap = new HashMap<>();
+        expectedMap.put(DOG, new Animal("wef", DOG, M, 10, 20, 1000, true));
+        expectedMap.put(Animal.Type.BIRD, new Animal("wef", Animal.Type.BIRD, M, 10, 20, 1000, true));
+        expectedMap.put(FISH, new Animal("Petya", FISH, F, 32, 53, 243, false));
+        expectedMap.put(Animal.Type.CAT, new Animal("Liza", Animal.Type.CAT, F, 30, 90, 432, true));
+        assertThat(tasks.heaviestAnimal(animals)).isEqualTo(expectedMap);
+    }
+
+    //task7
+    @Test
+    void oldestKTest() {
+        animals.add(new Animal("wef", Animal.Type.BIRD, M, 100, 20, 1000, true));
+        animals.add(new Animal("wef", Animal.Type.BIRD, M, 324, 20, 1000, true));
+        animals.add(new Animal("wef", Animal.Type.BIRD, M, 543, 20, 1000, true));
+        animals.add(new Animal("wef", Animal.Type.BIRD, M, 23, 20, 1000, true));
+        assertThat(tasks.oldestK(animals, 3)).isEqualTo(new Animal(
+            "wef",
+            Animal.Type.BIRD,
+            M,
+            100,
+            20,
+            1000,
+            true
+        ));
+    }
+
+    //task8
+    @Test
+    void heaviestAndBelowKTest() {
+        assertThat(tasks.haviestAnimalBelowK(animals, 100)).isEqualTo(Optional.of(new Animal(
+            "Liza",
+            Animal.Type.CAT,
+            F,
+            30,
+            90,
+            432,
+            true
+        )));
+    }
+
+    //task9
+    @Test
+    void countPawsTest() {
+        assertThat(tasks.countPaws(animals)).isEqualTo(10);
+    }
+
+    //task10
+    @Test
+    void animalsWithAgeNotEqualsPawsTest() {
+        animals.add(new Animal("wef", Animal.Type.BIRD, M, 2, 20, 1000, true));
+
+        List<Animal> expectedAnimals = createAnimals();
+        assertThat(tasks.AnimalsWithAgeNotEqualsPaws(animals)).isEqualTo(expectedAnimals);
+    }
+
+    //task11
+    @Test
+    void canBiteAndHigher100() {
+        animals.add(new Animal("wef", Animal.Type.BIRD, M, 2, 200, 1000, true));
+        animals.add(new Animal("wesfdf", Animal.Type.BIRD, M, 2, 300, 1000, true));
+        List<Animal> expectedAnimals = new ArrayList<>();
+        expectedAnimals.add(new Animal("wef", Animal.Type.BIRD, M, 2, 200, 1000, true));
+        expectedAnimals.add(new Animal("wesfdf", Animal.Type.BIRD, M, 2, 300, 1000, true));
+        assertThat(tasks.canBite(animals)).isEqualTo(expectedAnimals);
+    }
+
+    //task12
+    @Test
+    void weightMoreThanHeightTest() {
+        assertThat(tasks.weightMoreThanHeight(animals)).isEqualTo(3);
+    }
+
+    //task13
+    @Test
+    void nameWithMoreTwoWordsTest() {
+        animals.add(new Animal("wef fdw dsf", Animal.Type.BIRD, M, 2, 200, 1000, true));
+        animals.add(new Animal("wesfdf dsf sdf", Animal.Type.BIRD, M, 2, 300, 1000, true));
+        List<Animal> expectedAnimals = new ArrayList<>();
+        expectedAnimals.add(new Animal("wef fdw dsf", Animal.Type.BIRD, M, 2, 200, 1000, true));
+        expectedAnimals.add(new Animal("wesfdf dsf sdf", Animal.Type.BIRD, M, 2, 300, 1000, true));
+        assertThat(tasks.nameWithMoreTwoWords(animals)).isEqualTo(expectedAnimals);
+    }
+
+    //task14
+    @Test
+    void dogHigherThanK() {
+        animals.add(new Animal("wef fdw dsf", DOG, M, 2, 200, 1000, true));
+        assertThat(tasks.dogHigherThanK(animals, 100)).isTrue();
+        assertThat(tasks.dogHigherThanK(animals, 500)).isFalse();
+    }
+
+    //task15
+    @Test
+    void summarizeWeightWithConditionTest() {
+        animals.add(new Animal("wef fdw dsf", Animal.Type.CAT, M, 26, 200, 1000, true));
+        Map<Animal.Type, Integer> expectedMap = new HashMap<>();
+        expectedMap.put(FISH, 243);
+        expectedMap.put(Animal.Type.CAT, 1432);
+        assertThat(tasks.WeightFromKtoIlong(animals, 25, 40)).isEqualTo(expectedMap);
+    }
+
+    //task16
+    @Test
+    void sortTypeSexName() {
+        animals.add(new Animal("wef fdw dsf", Animal.Type.CAT, M, 26, 200, 1000, true));
+        System.out.println(tasks.SomeSorts(animals));
+        System.out.println();
+    }
+
+    //task17
+    @Test
+    void spiderBitesMoreThanDogTest() {
+        animals.add(new Animal("wef fdw dsf", SPIDER, M, 26, 200, 1000, true));
+        animals.add(new Animal("wef fdw dsf", DOG, M, 26, 200, 1000, false));
+        assertThat(tasks.isItTrueThatSpidersBiteMoreThanDogs(animals)).isTrue();
+    }
+
+    //task18
+    @Test
+    void findHeaviestFish() {
+        List<Animal> animals2 = new ArrayList<>();
+        animals.add(new Animal("Vasygaa", DOG, M, 10, 32, 364, true));
+        animals.add(new Animal("Petfweya", FISH, F, 32, 53, 264, false));
+        animals.add(new Animal("JorFWa", Animal.Type.BIRD, M, 53, 352, 53, false));
+        animals.add(new Animal("FW", FISH, F, 30, 90, 432, true));
+        assertThat(tasks.findHeaviestFishInLists(animals, animals2)).isEqualTo(new Animal(
+            "FW",
+            FISH,
+            F,
+            30,
+            90,
+            432,
+            true
+        ));
+
+
+    }
+}

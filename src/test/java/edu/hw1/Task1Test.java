13e5f275fe265335ef24d0c4de10631046ea8a5b
Big Shlepa <Andrsan2004@yandex.ru>
2023-11-11 21:27:09 +0300

    hw1 ctrl alt l commit

--- src/test/java/edu/hw1/Task1Test.java
+++ src/test/java/edu/hw1/Task1Test.java
@@ -5,18 +5,14 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class Task1Test {
-    @Test
-    @DisplayName("Ввод строки верного формата")
-    public void testValidInput() {
+    @Test @DisplayName("Ввод строки верного формата") public void testValidInput() {
         assertEquals(60, Task1.minutesToSeconds("01:00"));
         assertEquals(836, Task1.minutesToSeconds("13:56"));
         assertEquals(0, Task1.minutesToSeconds("00:00"));
         assertEquals(3599, Task1.minutesToSeconds("59:59"));
     }
 
-    @Test
-    @DisplayName("Ввод строки неверного формата")
-    public void testInvalidInput() {
+    @Test @DisplayName("Ввод строки неверного формата") public void testInvalidInput() {
         assertEquals(-1, Task1.minutesToSeconds("13:60")); // секунды >= 60
         assertEquals(-1, Task1.minutesToSeconds("abc:def")); // некорректный формат
         assertEquals(-1, Task1.minutesToSeconds("12:3a")); // некорректные числовые значения
@@ -26,9 +22,7 @@ public class Task1Test {
         assertEquals(-1, Task1.minutesToSeconds("5:-30")); // отрицательные секунды
     }
 
-    @Test
-    @DisplayName("Ввод строки null и пустой строки")
-    public void empty() {
+    @Test @DisplayName("Ввод строки null и пустой строки") public void empty() {
         assertEquals(-1, Task1.minutesToSeconds(null));
         assertEquals(-1, Task1.minutesToSeconds(""));
     }
--- src/test/java/edu/hw1/Task5Test.java
+++ src/test/java/edu/hw1/Task5Test.java
@@ -2,22 +2,16 @@ package edu.hw1;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertFalse;
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class Task5Test {
-    @Test
-    @DisplayName("Числовые значения")
-    public void numericValues() {
+    @Test @DisplayName("Числовые значения") public void numericValues() {
         assertEquals("11", Task5.Descendant(11211230));
         assertEquals("44", Task5.Descendant(13001120));
         assertEquals("5665", Task5.Descendant(23336014));
     }
 
-    @Test
-    @DisplayName("Срабатывание true и false")
-    public void TrueOrFalse() {
+    @Test @DisplayName("Срабатывание true и false") public void TrueOrFalse() {
         assertTrue(Task5.isPalindromeDescendant("11"));
         assertTrue(Task5.isPalindromeDescendant("44"));
         assertTrue(Task5.isPalindromeDescendant("5665"));

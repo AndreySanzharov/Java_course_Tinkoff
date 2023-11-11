629255a1e93fd723254a1523c403ca31f24acfa1
Big Shlepa <Andrsan2004@yandex.ru>
2023-11-11 23:50:53 +0300

    project1 update

---
+++ src/main/java/edu/project1/Dictionary.java
@@ -0,0 +1,6 @@
+package edu.project1;
+
+public class Dictionary {
+    public static final String[] WORDS = {"apple", "banana", "cherry", "date", "elderberry"};
+    public static final int MAX_MISTAKES = 5;
+}

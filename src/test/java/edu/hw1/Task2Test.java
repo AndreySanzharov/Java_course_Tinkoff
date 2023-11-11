6d8a39f30a259dd8d7b5600cd49f31c4eb3e2cc4
Big Shlepa <Andrsan2004@yandex.ru>
2023-11-11 21:11:28 +0300

    main teacher repo commit

---
+++ src/test/java/edu/hw1/Task2Test.java
@@ -0,0 +1,20 @@
+package edu.hw1;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class Task2Test {
+    @Test
+    @DisplayName("Любые числа")
+    public void CorrectItemsInput() {
+        assertEquals(4, Task2.countDigits(1234));
+        assertEquals(1, Task2.countDigits(0));
+        assertEquals(3, Task2.countDigits(-123));
+    }
+    @Test
+    @DisplayName("Число лонг формата")
+    public void ebeb(){
+        assertEquals(10, Task2.countDigits(3000000001L));
+    }
+}

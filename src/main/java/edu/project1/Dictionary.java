86dc700651edbc3d29f9e7192a6cd67f0904396a
Big Shlepa <Andrsan2004@yandex.ru>
2023-12-16 13:33:25 +0300

    hw9 fix

--- src/main/java/edu/project1/Dictionary.java
+++
@@ -1,9 +0,0 @@
-package edu.project1;
-
-public class Dictionary {
-    private Dictionary() {
-    }
-
-    public static final String[] WORDS = {"apple", "banana", "cherry", "date", "elderberry"};
-    public static final int MAX_MISTAKES = 5;
-}
